4ca8508ab37ff10a912ca876e4bf5e54385153a5
Tim Kendrick <timkendrick@gmail.com>
2017-04-17 15:07:48 +0100

    Allow manual clearing of memoized argument cache

--- test/spec/memoize.spec.js
+++ test/spec/memoize.spec.js
@@ -52,6 +52,48 @@ describe('memoize', () => {
           expect(actual).to.deep.equal(expected);
         });
       });
+
+      describe('AND the memoized function\'s cache is cleared', () => {
+        beforeEach(() => {
+          memoized.clear();
+        });
+
+        describe('AND the memoized function is called again', () => {
+          beforeEach(() => {
+            memoized();
+          });
+
+          it('SHOULD call the underlying function again', () => {
+            const actual = fn.callCount;
+            const expected = 2;
+            expect(actual).to.equal(expected);
+          });
+
+          it('SHOULD return the correct result', () => {
+            const actual = memoized.secondCall.returnValue;
+            const expected = 'foo';
+            expect(actual).to.deep.equal(expected);
+          });
+
+          describe('AND the memoized function is called again', () => {
+            beforeEach(() => {
+              memoized();
+            });
+
+            it('SHOULD NOT call the underlying function again', () => {
+              const actual = fn.callCount;
+              const expected = 2;
+              expect(actual).to.equal(expected);
+            });
+
+            it('SHOULD return the correct result', () => {
+              const actual = memoized.secondCall.returnValue;
+              const expected = 'foo';
+              expect(actual).to.deep.equal(expected);
+            });
+          });
+        });
+      });
     });
   });
 
@@ -111,6 +153,78 @@ describe('memoize', () => {
           it('SHOULD return the correct result', () => {
             const actual = memoized.thirdCall.returnValue;
             const expected = ['bar'];
+            expect(actual).to.deep.equal(expected);
+          });
+        });
+      });
+
+      describe('AND the memoized function\'s cache is cleared', () => {
+        beforeEach(() => {
+          memoized.clear();
+        });
+
+        describe('AND the memoized function is called again with the same argument', () => {
+          beforeEach(() => {
+            memoized('foo');
+          });
+
+          it('SHOULD call the underlying function again', () => {
+            const actual = fn.callCount;
+            const expected = 2;
+            expect(actual).to.equal(expected);
+          });
+
+          it('SHOULD return the correct result', () => {
+            const actual = memoized.secondCall.returnValue;
+            const expected = ['foo'];
+            expect(actual).to.deep.equal(expected);
+          });
+        });
+      });
+
+      describe('AND the memoized function\'s cache for that argument is cleared', () => {
+        beforeEach(() => {
+          memoized.clear('foo');
+        });
+
+        describe('AND the memoized function is called again with the same argument', () => {
+          beforeEach(() => {
+            memoized('foo');
+          });
+
+          it('SHOULD call the underlying function again', () => {
+            const actual = fn.callCount;
+            const expected = 2;
+            expect(actual).to.equal(expected);
+          });
+
+          it('SHOULD return the correct result', () => {
+            const actual = memoized.secondCall.returnValue;
+            const expected = ['foo'];
+            expect(actual).to.deep.equal(expected);
+          });
+        });
+      });
+
+      describe('AND the memoized function\'s cache for a different argument is cleared', () => {
+        beforeEach(() => {
+          memoized.clear('bar');
+        });
+
+        describe('AND the memoized function is called again with the same argument', () => {
+          beforeEach(() => {
+            memoized('foo');
+          });
+
+          it('SHOULD NOT call the underlying function again', () => {
+            const actual = fn.callCount;
+            const expected = 1;
+            expect(actual).to.equal(expected);
+          });
+
+          it('SHOULD return the correct result', () => {
+            const actual = memoized.secondCall.returnValue;
+            const expected = ['foo'];
             expect(actual).to.deep.equal(expected);
           });
         });
@@ -180,6 +294,79 @@ describe('memoize', () => {
           });
         });
       });
+
+      describe('AND the memoized function\'s cache is cleared', () => {
+        beforeEach(() => {
+          memoized.clear();
+        });
+
+        describe('AND the memoized function is called again with the same argument', () => {
+          beforeEach(() => {
+            memoized(foo);
+          });
+
+          it('SHOULD call the underlying function again', () => {
+            const actual = fn.callCount;
+            const expected = 2;
+            expect(actual).to.equal(expected);
+          });
+
+          it('SHOULD return the correct result', () => {
+            const actual = memoized.secondCall.returnValue;
+            const expected = [foo];
+            expect(actual).to.deep.equal(expected);
+          });
+        });
+      });
+
+      describe('AND the memoized function\'s cache for that argument is cleared', () => {
+        beforeEach(() => {
+          memoized.clear(foo);
+        });
+
+        describe('AND the memoized function is called again with the same argument', () => {
+          beforeEach(() => {
+            memoized(foo);
+          });
+
+          it('SHOULD call the underlying function again', () => {
+            const actual = fn.callCount;
+            const expected = 2;
+            expect(actual).to.equal(expected);
+          });
+
+          it('SHOULD return the correct result', () => {
+            const actual = memoized.secondCall.returnValue;
+            const expected = [foo];
+            expect(actual).to.deep.equal(expected);
+          });
+        });
+      });
+
+      describe('AND the memoized function\'s cache for a different argument is cleared', () => {
+        const bar = { bar: true };
+        beforeEach(() => {
+          memoized.clear(bar);
+        });
+
+        describe('AND the memoized function is called again with the same argument', () => {
+          beforeEach(() => {
+            memoized(foo);
+          });
+
+          it('SHOULD NOT call the underlying function again', () => {
+            const actual = fn.callCount;
+            const expected = 1;
+            expect(actual).to.equal(expected);
+          });
+
+          it('SHOULD return the correct result', () => {
+            const actual = memoized.secondCall.returnValue;
+            const expected = [foo];
+            expect(actual).to.deep.equal(expected);
+          });
+        });
+      });
     });
   });
 
@@ -239,6 +426,102 @@ describe('memoize', () => {
           it('SHOULD return the correct result', () => {
             const actual = memoized.thirdCall.returnValue;
             const expected = ['foo', 'bar', 'qux'];
+            expect(actual).to.deep.equal(expected);
+          });
+        });
+      });
+
+      describe('AND the memoized function\'s cache is cleared', () => {
+        beforeEach(() => {
+          memoized.clear();
+        });
+
+        describe('AND the memoized function is called again with the same arguments', () => {
+          beforeEach(() => {
+            memoized('foo', 'bar', 'baz');
+          });
+
+          it('SHOULD call the underlying function again', () => {
+            const actual = fn.callCount;
+            const expected = 2;
+            expect(actual).to.equal(expected);
+          });
+
+          it('SHOULD return the correct result', () => {
+            const actual = memoized.secondCall.returnValue;
+            const expected = ['foo', 'bar', 'baz'];
+            expect(actual).to.deep.equal(expected);
+          });
+        });
+      });
+
+      describe('AND the memoized function\'s cache for that set of arguments is cleared', () => {
+        beforeEach(() => {
+          memoized.clear('foo', 'bar', 'baz');
+        });
+
+        describe('AND the memoized function is called again with the same arguments', () => {
+          beforeEach(() => {
+            memoized('foo', 'bar', 'baz');
+          });
+
+          it('SHOULD call the underlying function again', () => {
+            const actual = fn.callCount;
+            const expected = 2;
+            expect(actual).to.equal(expected);
+          });
+
+          it('SHOULD return the correct result', () => {
+            const actual = memoized.secondCall.returnValue;
+            const expected = ['foo', 'bar', 'baz'];
+            expect(actual).to.deep.equal(expected);
+          });
+        });
+      });
+
+      describe('AND the memoized function\'s cache for a partial set of arguments is cleared', () => {
+        beforeEach(() => {
+          memoized.clear('foo', 'bar');
+        });
+
+        describe('AND the memoized function is called again with the same argument', () => {
+          beforeEach(() => {
+            memoized('foo', 'bar', 'baz');
+          });
+
+          it('SHOULD call the underlying function again', () => {
+            const actual = fn.callCount;
+            const expected = 2;
+            expect(actual).to.equal(expected);
+          });
+
+          it('SHOULD return the correct result', () => {
+            const actual = memoized.secondCall.returnValue;
+            const expected = ['foo', 'bar', 'baz'];
+            expect(actual).to.deep.equal(expected);
+          });
+        });
+      });
+
+      describe('AND the memoized function\'s cache for a different set of arguments is cleared', () => {
+        beforeEach(() => {
+          memoized.clear('foo', 'bar', 'qux');
+        });
+
+        describe('AND the memoized function is called again with the same argument', () => {
+          beforeEach(() => {
+            memoized('foo', 'bar', 'baz');
+          });
+
+          it('SHOULD NOT call the underlying function again', () => {
+            const actual = fn.callCount;
+            const expected = 1;
+            expect(actual).to.equal(expected);
+          });
+
+          it('SHOULD return the correct result', () => {
+            const actual = memoized.secondCall.returnValue;
+            const expected = ['foo', 'bar', 'baz'];
             expect(actual).to.deep.equal(expected);
           });
         });
@@ -310,6 +593,103 @@ describe('memoize', () => {
           });
         });
       });
+
+      describe('AND the memoized function\'s cache is cleared', () => {
+        beforeEach(() => {
+          memoized.clear();
+        });
+
+        describe('AND the memoized function is called again with the same arguments', () => {
+          beforeEach(() => {
+            memoized(foo, bar, baz);
+          });
+
+          it('SHOULD call the underlying function again', () => {
+            const actual = fn.callCount;
+            const expected = 2;
+            expect(actual).to.equal(expected);
+          });
+
+          it('SHOULD return the correct result', () => {
+            const actual = memoized.secondCall.returnValue;
+            const expected = [foo, bar, baz];
+            expect(actual).to.deep.equal(expected);
+          });
+        });
+      });
+
+      describe('AND the memoized function\'s cache for that set of arguments is cleared', () => {
+        beforeEach(() => {
+          memoized.clear(foo, bar, baz);
+        });
+
+        describe('AND the memoized function is called again with the same arguments', () => {
+          beforeEach(() => {
+            memoized(foo, bar, baz);
+          });
+
+          it('SHOULD call the underlying function again', () => {
+            const actual = fn.callCount;
+            const expected = 2;
+            expect(actual).to.equal(expected);
+          });
+
+          it('SHOULD return the correct result', () => {
+            const actual = memoized.secondCall.returnValue;
+            const expected = [foo, bar, baz];
+            expect(actual).to.deep.equal(expected);
+          });
+        });
+      });
+
+      describe('AND the memoized function\'s cache for a partial set of arguments is cleared', () => {
+        beforeEach(() => {
+          memoized.clear(foo, bar);
+        });
+
+        describe('AND the memoized function is called again with the same argument', () => {
+          beforeEach(() => {
+            memoized(foo, bar, baz);
+          });
+
+          it('SHOULD call the underlying function again', () => {
+            const actual = fn.callCount;
+            const expected = 2;
+            expect(actual).to.equal(expected);
+          });
+
+          it('SHOULD return the correct result', () => {
+            const actual = memoized.secondCall.returnValue;
+            const expected = [foo, bar, baz];
+            expect(actual).to.deep.equal(expected);
+          });
+        });
+      });
+
+      describe('AND the memoized function\'s cache for a different set of arguments is cleared', () => {
+        const qux = { qux: true };
+        beforeEach(() => {
+          memoized.clear(foo, bar, qux);
+        });
+
+        describe('AND the memoized function is called again with the same argument', () => {
+          beforeEach(() => {
+            memoized(foo, bar, baz);
+          });
+
+          it('SHOULD NOT call the underlying function again', () => {
+            const actual = fn.callCount;
+            const expected = 1;
+            expect(actual).to.equal(expected);
+          });
+
+          it('SHOULD return the correct result', () => {
+            const actual = memoized.secondCall.returnValue;
+            const expected = [foo, bar, baz];
+            expect(actual).to.deep.equal(expected);
+          });
+        });
+      });
     });
   });
 
@@ -354,6 +734,102 @@ describe('memoize', () => {
           const actual = memoized.secondCall.returnValue;
           const expected = [null, undefined, false];
           expect(actual).to.deep.equal(expected);
+        });
+      });
+
+      describe('AND the memoized function\'s cache is cleared', () => {
+        beforeEach(() => {
+          memoized.clear();
+        });
+
+        describe('AND the memoized function is called again with the same arguments', () => {
+          beforeEach(() => {
+            memoized(null, undefined, false);
+          });
+
+          it('SHOULD call the underlying function again', () => {
+            const actual = fn.callCount;
+            const expected = 2;
+            expect(actual).to.equal(expected);
+          });
+
+          it('SHOULD return the correct result', () => {
+            const actual = memoized.secondCall.returnValue;
+            const expected = [null, undefined, false];
+            expect(actual).to.deep.equal(expected);
+          });
+        });
+      });
+
+      describe('AND the memoized function\'s cache for that set of arguments is cleared', () => {
+        beforeEach(() => {
+          memoized.clear(null, undefined, false);
+        });
+
+        describe('AND the memoized function is called again with the same arguments', () => {
+          beforeEach(() => {
+            memoized(null, undefined, false);
+          });
+
+          it('SHOULD call the underlying function again', () => {
+            const actual = fn.callCount;
+            const expected = 2;
+            expect(actual).to.equal(expected);
+          });
+
+          it('SHOULD return the correct result', () => {
+            const actual = memoized.secondCall.returnValue;
+            const expected = [null, undefined, false];
+            expect(actual).to.deep.equal(expected);
+          });
+        });
+      });
+
+      describe('AND the memoized function\'s cache for a partial set of arguments is cleared', () => {
+        beforeEach(() => {
+          memoized.clear(null, undefined);
+        });
+
+        describe('AND the memoized function is called again with the same argument', () => {
+          beforeEach(() => {
+            memoized(null, undefined, false);
+          });
+
+          it('SHOULD call the underlying function again', () => {
+            const actual = fn.callCount;
+            const expected = 2;
+            expect(actual).to.equal(expected);
+          });
+
+          it('SHOULD return the correct result', () => {
+            const actual = memoized.secondCall.returnValue;
+            const expected = [null, undefined, false];
+            expect(actual).to.deep.equal(expected);
+          });
+        });
+      });
+
+      describe('AND the memoized function\'s cache for a different set of arguments is cleared', () => {
+        beforeEach(() => {
+          memoized.clear(null, undefined, 0);
+        });
+
+        describe('AND the memoized function is called again with the same argument', () => {
+          beforeEach(() => {
+            memoized(null, undefined, false);
+          });
+
+          it('SHOULD NOT call the underlying function again', () => {
+            const actual = fn.callCount;
+            const expected = 1;
+            expect(actual).to.equal(expected);
+          });
+
+          it('SHOULD return the correct result', () => {
+            const actual = memoized.secondCall.returnValue;
+            const expected = [null, undefined, false];
+            expect(actual).to.deep.equal(expected);
+          });
         });
       });
     });
